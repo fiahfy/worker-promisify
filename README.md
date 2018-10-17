86d2a3f7718af68b61972deeca70b2db041d4a20
fiahfy <d.fiahfy@gmail.com>
2018-10-17 14:31:15 +0900

    Use in parallel

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ npm install @fiahfy/worker-promisify
 ```
 
 ## Usage
+`worker.js`
+```js
+onmessage = (e) => {
+  postMessage('pong')
+}
+```
+`main.js`
 ```js
 import promisify from '@fiahfy/worker-promisify'
 
@@ -15,6 +22,29 @@ const worker = new Worker('worker.js')
 const promiseWorker = promisify(worker)
 
 promiseWorker.postMessage('ping').then((e) => {
-  console.log(e.data)
+  console.log(e.data) // pong
+})
+```
+
+### Parallel execution
+`worker.js`
+```js
+onmessage = ({ data: { key, data } }) => {
+  postMessage({ key, data: Math.pow(data, 2) }) // must pass key and set return value to data
+}
+```
+`main.js`
+```js
+import promisify from '@fiahfy/worker-promisify'
+
+const worker = new Worker('worker.js')
+const promiseWorker = promisify(worker)
+
+Promise.all([
+  promiseWorker.postMessage({ key: 'foo', data: 2 }), // must set unique keys
+  promiseWorker.postMessage({ key: 'bar', data: 3 })
+]).then(([e1, e2]) => {
+  console.log(e1.data) // 4
+  console.log(e2.data) // 9
 })
 ```
--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,9 @@ import promisify from '../src'
 const code = `
   onmessage = ({ data }) => {
     if (isNaN(data)) {
-      throw new Error('Not a Number')
+      throw new TypeError('Not a Number')
     }
-    postMessage(data * 2)
+    postMessage(Math.pow(data, 2))
   }
 `
 const worker = new Worker(URL.createObjectURL(new Blob([code])))
@@ -24,20 +24,53 @@ describe('promisify', () => {
 
 const promiseWorker = promisify(worker)
 
-describe('promise worker', () => {
+describe('terminate', () => {
+  test('should work', () => {
+    // not supported on jsdom-worker
+    expect(() => promiseWorker.terminate()).toThrowError('Not Supported')
+  })
+})
+
+describe('postMessage', () => {
   test('should work', async () => {
     const e = await promiseWorker.postMessage(2)
     expect(e.data).toBe(4)
   })
 
   test('should work on reject', async () => {
-    await expect(promiseWorker.postMessage(NaN)).rejects.toThrowError(
-      'Not a Number'
-    )
+    await expect(promiseWorker.postMessage(NaN)).rejects.toThrowError(TypeError)
+  })
+})
+
+const code2 = `
+  onmessage = ({ data: { key, data } }) => {
+    if (isNaN(data)) {
+      throw new TypeError('Not a Number')
+    }
+    setTimeout(() => {
+      postMessage({ key, data })
+    }, data)
+  }
+`
+const worker2 = new Worker(URL.createObjectURL(new Blob([code2])))
+const delayWorker = promisify(worker2)
+
+describe('postMessage in parallel', () => {
+  test('should work', async () => {
+    const [e1, e2] = await Promise.all([
+      delayWorker.postMessage({ key: 'foo', data: 100 }),
+      delayWorker.postMessage({ key: 'bar', data: 200 })
+    ])
+    expect(e1.data).toBe(100)
+    expect(e2.data).toBe(200)
   })
 
-  test('should work terminate', () => {
-    // not supported on jsdom-worker
-    expect(() => promiseWorker.terminate()).toThrowError('Not Supported')
+  test('should work on reject', async () => {
+    await expect(
+      Promise.all([
+        delayWorker.postMessage({ key: 'foo', data: NaN }),
+        delayWorker.postMessage({ key: 'bar', data: 200 })
+      ])
+    ).rejects.toThrowError(TypeError)
   })
 })
